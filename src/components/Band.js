00e883dbcfba9672320bc9144cfb826f03910890
Lara Sanders <larasanders@gmail.com>
2019-07-28 23:41:49 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -5,7 +5,7 @@ class Band extends Component {
   render() {
     return(
       <div>
-    <span>{props.band.text}</span><button onClick={() => props.delete(props.band.id)}>DELETE</button>
+    <span>{props.band.bandNAme}</span><button onClick={() => props.delete(props.band.id)}>DELETE</button>
   </div>
     );
   }
